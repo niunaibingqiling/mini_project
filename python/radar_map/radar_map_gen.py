25f5235cab730e4c27a1bf85d4f3618b7c81d3a3
liding <2906965429@qq.com>
2025-12-03 00:09:39 +0800

    添加了中文库

--- python/radar_map/radar_map_gen.py
+++ python/radar_map/radar_map_gen.py
@@ -2,10 +2,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib
-a=sorted([f.name for f in matplotlib.font_manager.fontManager.ttflist])
 
-for i in a:
-    print(i)
+zhfont1  =   matplotlib.font_manager.FontProperties(fname="SourceHanSerifSC-Regular.otf")
 
 results  =   [{"大学英语": 87, "高等数学": 79, "体育": 95, "计算机基础": 92, "程序设计": 85},
                 {"大学英语": 80, "高等数学": 90, "体育": 91, "计算机基础": 85, "程序设计": 88}]
@@ -37,7 +35,7 @@ ax.plot(angles,score_b,color='b')
 # 设置极坐标图的角度刻度网格线和对应的标签文本
 # angles*180/np.pi：将弧度转换为角度（0-360度）
 # labels：对应每个角度的标签文本（字符串列表）
-ax.set_thetagrids(angles*180/np.pi,labels)
+ax.set_thetagrids(angles*180/np.pi,labels,fontproperties=zhfont1)
 
 
 # 设置雷达图的坐标刻度范围
@@ -50,8 +48,8 @@ plt.axis('off')
 
 # 设置雷达图的坐标值显示角度，相对于起始角度的偏移量
 # ax.set_rlabel_position(0)
-ax.set_title("计算机专业大一")
-plt.legend(["上学期","下学期"],loc='best')
+ax.set_title("计算机专业大一", fontproperties=zhfont1)
+plt.legend(["上学期","下学期"],prop=zhfont1)
 
 plt.show()
 
